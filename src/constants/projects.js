661bda153043c498dffe7ad238b782c455f7d44b
Francis Ong <francis.ong@stratpoint.com>
2023-05-07 22:40:10 +0800

    feat: modify projects constants

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,42 +1,39 @@
 export const PROJECTS = [
   {
-    title: 'MERN Memories',
-    description:
-      "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-    image: '/images/1.png',
-    tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
     id: 0,
-  },
-  {
-    title: 'E-Commerce',
-    description:
-      "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
+    image: '/assets/projects/huremas_home.png',
+    tags: ['HTML', 'CSS', 'Bootstrap', 'PHP', 'MySQL', 'Javascript'],
+    title: 'HUREMAS - CVSUIC',
+    description: 'Huremas Desc',
     source: 'https://google.com',
     visit: 'https://google.com',
+  },
+
+  {
     id: 1,
-  },
-  {
-    title: 'WebRTC App',
-    description:
-      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-    image: '/images/3.jpg',
-    tags: ['React', 'WebRTC'],
+    image: '/assets/projects/rms_login.png',
+    tags: ['Python (PyQT5)', 'MySQL'],
+    title: 'RMEEC - Record Management System',
+    description: 'RMEEEC',
     source: 'https://google.com',
     visit: 'https://google.com',
+  },
+  {
     id: 2,
-  },
-  {
-    title: 'Unichat',
-    description:
-      'This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application',
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
+    image: '/assets/projects/netflix_clone.png',
+    tags: ['React', 'Javascript', 'Axios', 'CSS'],
+    title: 'Netflix Clone',
+    description: 'Netflix',
     source: 'https://google.com',
     visit: 'https://google.com',
+  },
+  {
     id: 3,
+    image: '/assets/projects/splendid_food.png',
+    tags: ['Vue', 'Javascript', 'CSS'],
+    title: 'Splendid Food',
+    description: 'Splendid',
+    source: 'https://google.com',
+    visit: 'https://google.com',
   },
 ];
