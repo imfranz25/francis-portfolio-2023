d3d40830b0b70b52c2278d5e5823c673712e377b
Francis Ong <francis.ong@stratpoint.com>
2023-05-07 21:28:48 +0800

    feat: add proojects, accomplishments, career timeeline constants

---
+++ src/constants/accomplishments.js
@@ -0,0 +1,6 @@
+export const ACCOMPLISHMENTS = [
+  { number: 20, text: 'Open Source Projects' },
+  { number: 1000, text: 'Students' },
+  { number: 1900, text: 'Github Followers' },
+  { number: 5000, text: 'Github Stars' },
+];
